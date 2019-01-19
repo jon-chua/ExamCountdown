753e1265f4b9da49a6ef3627f090949e0b9caac5
Jon Chua <jonchua97@gmail.com>
2019-01-19 18:50:33 +0800

    Added NUSMods Exam Calendar

--- app/src/main/java/com/example/examcountdown/MainActivity.java
+++ app/src/main/java/com/example/examcountdown/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.examcountdown;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    BufferedReader reader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +26,20 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final TextView textView = findViewById(R.id.textView);
+        InputStream is = this.getResources().openRawResource(R.raw.data);
+        reader = new BufferedReader(new InputStreamReader(is));
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                try {
+                    textView.setText(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
